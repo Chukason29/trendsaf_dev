c35dcb3d384736657362d1c9ea653a4c0af63ee9
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2025-02-06 18:26:14 +0100

    sign up issue

--- app/blueprints/signup/routes.py
+++ app/blueprints/signup/routes.py
@@ -198,12 +198,12 @@ def confirm_email(token):
                 token_filter.is_token_used = True
                 db.session.commit()
                 #return redirect ('http://localhost:5173/success')
-                return redirect("https://app.trendsaf.co/confirm_email?status=True&message=success")
+                return redirect(f"{Config.BASE_URL}/confirm_email?status=True&message=success")
         else:
-            return redirect("https://app.trendsaf.co/confirm_email?status=False&message=link has been used")
+            return redirect(f"{Config.BASE_URL}/confirm_email?status=False&message=link has been used")
     except:
         db.session.rollback()
-        return redirect("https://app.trendsaf.co/confirm_email?status=False&message=link has expired")
+        return redirect(f"{Config.BASE_URL}/confirm_email?status=False&message=link has expired")
         
 @signup_bp.route('/')
 def index():
